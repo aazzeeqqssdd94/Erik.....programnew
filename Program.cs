28d04cba185413ac60622d523e37362e9d83abcf
Opilane TTHK <opilane@tthk.ee>
2025-09-10 09:39:12 +0300

    c#

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+int arv1 = 100;
+float arv2 = 25;
+int arv3 = 50;
+int tulemus = arv1 + arv3;
+Console.WriteLine(tulemus + tulemus);
+Console.WriteLine("https://meet.gogle.com/xjy-drff-qbf");
+string tekst1 = "mingisugune tekst";
+string tekst2 = "AM THIRST,gib water";//kasutajale esitav tekst 
+Console.WriteLine(tekst1 + tekst2);
+bool thisthing = false;//mingisume olek
+Console. WriteLine(thisthing);
+string yourname = Console.ReadLine();
+Console.WriteLine("tere hommikust,õpilane:?"+yourname);
+*/
+Console.WriteLine("Tere. Sisesta esimene liidetav arv");
+int arv1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Tere. Sisesta esimene liidetav arv");
+int arv2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Tere.Sisesta teine liidetav arv");
+string tehteyyp = (Console.ReadLine());
+
+int tulemus = 0;
+if (tehtetyyp == "+")
+
+    tulemus = arv1 + arv2;
+
+
+    Console.WriteLine("sisesta esimene jagatav");
+    int jagatav1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("sisesta teine jagaja");
+    int jagatava2 = int.Parse(Console.ReadLine());
+    int jagamine = jagatav1 / jagatava2;
+    Console.WriteLine(jagamine);
+
+
+/* ISESEISEV TUNNI ÜLESANNE: */
+/* Kirjuta ka jagamis, korrutamis ning lahutustehted liitmise eeskujul*/
+
+// + liitmine
+// / jagamine
+// * korrutamine
+// - lahutamine
